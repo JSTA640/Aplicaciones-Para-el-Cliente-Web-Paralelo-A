85694829dc22bf65a9d90bc2a656084aa1ccde35
JSTA640 <jennytigua353@gmail.com>
2025-09-23 23:05:54 -0500

    HTML

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+// Este archivo exporta tipos e interfaces que pueden ser utilizados en el proyecto.
+
+export interface FormData {
+    name: string;
+    email: string;
+    url?: string;
+    date?: string;
+    time?: string;
+    datetime?: string;
+    month?: string;
+    week?: string;
+    number: number;
+    tel?: string;
+    search?: string;
+    color?: string;
+}
