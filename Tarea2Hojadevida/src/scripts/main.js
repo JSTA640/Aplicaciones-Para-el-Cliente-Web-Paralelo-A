a310a94b2eb6b22a2f7b065917f8130073bd3235
JSTA640 <jennytigua353@gmail.com>
2025-09-28 23:35:35 -0500

    Actualizacion de hoja de vida

--- Tarea2Hojadevida/src/scripts/main.js
+++
@@ -1,5 +0,0 @@
-// main.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Aquí puedes agregar interactividad a la hoja de vida si es necesario.
-    console.log("La hoja de vida está lista.");
-});
